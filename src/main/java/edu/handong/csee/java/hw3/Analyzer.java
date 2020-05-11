f8e7302dc52ee24f65d75c7c04d1500ac2f907d3
Jihyoung Jang <59951756+wlgud2757@users.noreply.github.com>
2020-05-12 02:06:16 +0900

    first commit

--- src/main/java/edu/handong/csee/java/hw3/Analyzer.java
+++ src/main/java/edu/handong/csee/java/hw3/Analyzer.java
@@ -1,5 +1,33 @@
 package edu.handong.csee.java.hw3;
 
 public class Analyzer {
-
+	
+	Analyzer(String[] analyzerData) {
+		
+	}
+	
+	public int getNumberOfCountries() {
+		return 1;
+	}
+	
+	public int getNumberOfAllPatients() {
+		return 1;
+	}
+	
+	public int getNumberOfPatientsOfACountry(String country) {
+		return 1;
+	}
+	
+	public int getNumberOfPatientsFromASpecifiedDate(String fromDate) {
+		return 1;
+	}
+	
+	public int getNumberOfPatientsBeforeASpecifiedDate(String specifiedDate) {
+		return 1;
+	}
+	
+	public int getNumberOfPatientsBetweenTwoDates(String date1, String date2) {
+		return 1;
+	}
+	
 }
